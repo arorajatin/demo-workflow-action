d23069c9cb77a194b8d0bf97e2ebe80382a5330c
Jatin Arora <jatin@utilize.app>
2022-07-10 20:49:46 +0530

    Update workflow.py

--- workflow.py
+++ workflow.py
@@ -41,14 +41,9 @@ from obsei.source.twitter_source import TwitterCredentials, TwitterSource, Twitt
 
 # initialize twitter source config
 source_config = TwitterSourceConfig(
-   keywords=["looking for nocode"], # Keywords, @user or #hashtags
-   lookup_period="6h", # Lookup period from current time, format: `<number><d|h|m>` (day|hour|minute)
-   cred_info=TwitterCredentials(
-       # Enter your twitter consumer key and secret. Get it from https://developer.twitter.com/en/apply-for-access
-       consumer_key=os.getenv("TWITTER_CONSUMER_KEY"),
-       consumer_secret=os.getenv("TWITTER_CONSUMER_SECRET"),
-       bearer_token=os.getenv("TWITTER_BEARER_TOKEN"),
-   )
+   keywords=["looking for nocode"],
+   lookup_period="6h",
+   cred_info=TwitterCredentials(consumer_key=os.getenv("TWITTER_CONSUMER_KEY"),consumer_secret=os.getenv("TWITTER_CONSUMER_SECRET"),bearer_token=os.getenv("TWITTER_BEARER_TOKEN"),)
 )
 
 # initialize tweets retriever
